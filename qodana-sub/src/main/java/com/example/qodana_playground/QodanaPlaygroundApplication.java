06ef55a4353ff5f0a686b58eefb809b91dd78637
kazuma-saito-rakus <kazuma.saito@rakus.co.jp>
2025-09-19 09:10:40 +0900

    first commit

---
+++ qodana-sub/src/main/java/com/example/qodana_playground/QodanaPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.qodana_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QodanaPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QodanaPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/qodana_playground/QodanaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qodana_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QodanaPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
