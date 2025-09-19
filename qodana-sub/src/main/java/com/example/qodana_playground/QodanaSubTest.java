e6a3186972c76196667183bc926f15d290fbaece
kazuma-saito-rakus <kazuma.saito@rakus.co.jp>
2025-09-19 09:25:41 +0900

    feat: qodanaの設定を追加

---
+++ qodana-sub/src/main/java/com/example/qodana_playground/QodanaSubTest.java
@@ -0,0 +1,8 @@
+package com.example.qodana_playground;
+
+public class QodanaSubTest {
+  public static void main(String[] args) {
+    String s = null;
+    System.out.println(s.length());  // NullPointerExceptionが発生する
+  }
+}
