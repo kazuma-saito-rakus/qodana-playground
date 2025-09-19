d1c80ef05c822476f8fe58af4e2905d7f2f7c91c
kazuma-saito-rakus <kazuma.saito@rakus.co.jp>
2025-09-19 09:26:14 +0900

    feat: qodanaの設定を追加

---
+++ src/main/java/com/example/qodana_playground/QodanaTest.java
@@ -0,0 +1,8 @@
+package com.example.qodana_playground;
+
+public class QodanaTest {
+  public static void main(String[] args) {
+    String s = null;
+    System.out.println(s.length());  // NullPointerExceptionが発生する
+  }
+}
